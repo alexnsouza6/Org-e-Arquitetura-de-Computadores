3b5923123fb043440ac639671661b3d71ea0bc4f
alexnsouza6 <alexnsouza6@hotmail.com>
2017-09-09 12:34:00 -0300

    Finished load function and add menu for users

--- Trabalho1/Sim.c
+++ Trabalho1/Sim.c
@@ -17,7 +17,7 @@ int32_t lw(uint32_t address, int16_t kte){
 	/* Lê um inteiro alinhado - endereços múltiplos de 4 */
 	int32_t pos;
 
-	if(address%4!=0) printf("Invalid Address \n");
+	if((address + (int32_t) kte)%4!=0) printf("Invalid Address \n");
 	else pos = (address + (int32_t) kte)/4;
 	return mem[pos];
 	
@@ -52,32 +52,34 @@ int32_t lh(uint32_t address, int16_t kte){
 int32_t lb(uint32_t address, int16_t kte){
 	int32_t	pos = (address + kte)/4;
 	int32_t	res = (address + kte)%4;
-	int32_t	word = mem[pos];
-	int8_t	*byte;
+	int32_t	word = mem[pos], word_aux = mem[pos];
 
 	switch(res){
 		case 0:
 			word = word & 0x000000ff;
-			*byte = word & 0x000000f0;
-			if(*byte>0x7) word = word | 0xffffffff;
+			word_aux = word >> 7;
+			if(word_aux == 1) word = word | 0xffffff00;
+			
 			break;
 		case 1:
-			word = word << 8;
+			word = word >> 8;
 			word = word & 0x000000ff;
-			*byte = word & 0x000000f0;
-			if(*byte>0x7) word = word | 0xffffffff;
+			word_aux = word >> 7;
+			if(word_aux == 1) word = word | 0xffffff00;
+			
 			break;
 		case 2:
-			word = word << 16;
+			word = word >> 16;
 			word = word & 0x000000ff;
-			*byte = word & 0x000000f0;
-			if(*byte>0x7) word = word | 0xffffffff;
+			word_aux = word >> 7;
+			if(word_aux == 1) word = word | 0xffffff00;
+			
 			break;
 		case 3:
-			word = word << 24;
+			word = word >> 24;
 			word = word & 0x000000ff;
-			*byte = word & 0x000000f0;
-			if(*byte>0x7) word = word | 0xffffffff;
+			word_aux = word >> 7;
+			if(word_aux == 1) word = word | 0xffffff00;
 		break;
 		default:
 			printf("Different from expected - Error loading byte");
@@ -88,47 +90,60 @@ int32_t lb(uint32_t address, int16_t kte){
 
 //sw $t1,-100($t2)        Store word : Store contents of $t1 into effective memory word address
 void sw(uint32_t address, int16_t kte, int32_t dado){
-	int32_t pos = (address + kte)/4;
+	int32_t pos;
+
+	if((address + (int32_t) kte)%4!=0) printf("Invalid Address \n");
+	else pos = (address + (int32_t) kte)/4;
 	mem[pos] = dado;
 }
 
 
 //sh $t1,-100($t2)        Store halfword : Store the low-order 16 bits of $t1 into the effective memory halfword address
 void sh(uint32_t address, int16_t kte, int16_t dado){
-	int32_t pos = (address+kte)/4;
-	int32_t res	= (address+kte)%4;
-	int32_t word = mem[pos];
+	int32_t pos = (address+(int32_t)kte)/4;
+	int32_t res	= (address+(int32_t)kte)%4;
+	int32_t word = 0x0;
+	word += (int32_t) dado;
+
 
 	if(res==0){	
-		dado = dado & 0x0000ffff;
-		mem[pos] = dado;
+		mem[pos] = mem[pos] & 0xffff0000;
+		mem[pos] += word;
 	}
 	else{	
+		mem[pos] = mem[pos] & 0x0000ffff;
 		word = word << 16;
-		mem[pos] = dado;
+		mem[pos] += word;
 	}
 }
 
 //sb $t1,-100($t2)        Store byte : Store the low-order 8 bits of $t1 into the effective memory byte address
 void sb(uint32_t address, int16_t kte, int8_t dado){
-	int32_t	pos = (address + kte)/4;
-	int32_t	res = (address + kte)%4;
+	int32_t	pos = (address + (int32_t)kte)/4;
+	int32_t	res = (address + (int32_t)kte)%4;
+	int32_t word = 0x0;
+
+	word += (int32_t) dado;
 
 	switch(res){
 		case 0:
-			dado = dado & 0x000000ff;
+			mem[pos] = mem[pos] & 0x000000ff;
+			mem[pos] += word;
 			break;
 		case 1:
-			dado = dado << 8;
-			dado = dado & 0x000000ff;
+			mem[pos] = mem[pos] << 8;
+			mem[pos] = mem[pos] & 0x000000ff;
+			mem[pos] += word;
 			break;
 		case 2:
-			dado = dado << 16;
-			dado = dado & 0x000000ff;
+			mem[pos] = mem[pos] << 16;
+			mem[pos] = mem[pos] & 0x000000ff;
+			mem[pos] += word;
 			break;
 		case 3:
-			dado = dado << 24;
-			dado = dado & 0x000000ff;
+			mem[pos] = mem[pos] << 24;
+			mem[pos] = mem[pos] & 0x000000ff;
+			mem[pos] += word;
 			break;
 		default:
 			printf("Different from expected - Error loading byte");
